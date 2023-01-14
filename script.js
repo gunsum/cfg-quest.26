22e62e8b480ccac42c307f5d7e13ac5b037beeee
Fatma Oymak <bedizma@hotmail.com>
2023-01-14 00:13:27 +0000

    answer is

---
+++ script.js
@@ -0,0 +1,10 @@
+moviesAndTvShowsToWatch = [
+    "Howl's Moving Castle",
+    "Community",
+    "Princess Mononoke",
+    "The Good Place,"
+]
+
+moviesAndTvShowsToWatch.splice(2, 0, "The Umbrella Academy")
+
+console.log(moviesAndTvShowsToWatch);
